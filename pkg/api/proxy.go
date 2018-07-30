2234ffe67b2d6ac02c638795ea632808758bb7ae
Stefan Majewsky <stefan.majewsky@sap.com>
2018-07-30 13:01:19 +0200

    fix copy-paste error in copyright headers

--- pkg/api/proxy.go
+++ pkg/api/proxy.go
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-*  Copyright 2018 Stefan Majewsky <majewsky@gmx.net>
+*  Copyright 2018 SAP SE
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
 *  you may not use this file except in compliance with the License.
