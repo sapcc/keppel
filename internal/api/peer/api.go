563f6bf40dbe77b7a8ffc08ec2476206aa970001
Stefan Majewsky <stefan.majewsky@sap.com>
2021-11-15 15:25:38 +0100

    remove obsolete endpoint: GET /peer/v1/account-filter/:account

--- internal/api/peer/api.go
+++ internal/api/peer/api.go
@@ -23,8 +23,6 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/sapcc/go-bits/respondwith"
-	"github.com/sapcc/go-bits/sre"
 	"github.com/sapcc/keppel/internal/auth"
 	"github.com/sapcc/keppel/internal/keppel"
 )
@@ -49,7 +47,6 @@ func (a *API) AddTo(r *mux.Router) {
 	//to upstream, so there is an additional /v2/ in there in reference to the
 	//Registry V2 API.
 	r.Methods("GET").Path("/peer/v1/delegatedpull/{hostname}/v2/{repo:.+}/manifests/{reference}").HandlerFunc(a.handleDelegatedPullManifest)
-	r.Methods("GET").Path("/peer/v1/account-filter/{account}").HandlerFunc(a.handleAccountFilter)
 	r.Methods("POST").Path("/peer/v1/sync-replica/{account}/{repo:.+}").HandlerFunc(a.handleSyncReplica)
 }
 
@@ -77,29 +74,4 @@ func (a *API) authenticateRequest(w http.ResponseWriter, r *http.Request) *keppe
 	}
 
 	return &peer
-}
-
-// AccountFilter endpoint json struct
-type AccountFilter struct {
-	Filter keppel.PlatformFilter
-}
-
-func (a *API) handleAccountFilter(w http.ResponseWriter, r *http.Request) {
-	sre.IdentifyEndpoint(r, "/peer/v1/account-filter/:account")
-	peer := a.authenticateRequest(w, r)
-	if peer == nil {
-		return
-	}
-
-	//find account
-	account, err := keppel.FindAccount(a.db, mux.Vars(r)["account"])
-	if respondwith.ErrorText(w, err) {
-		return
-	}
-	if account == nil {
-		http.Error(w, "not found", http.StatusNotFound)
-		return
-	}
-
-	respondwith.JSON(w, http.StatusOK, AccountFilter{Filter: account.PlatformFilter})
 }
