d08a17bedfc767cde19a675f9ff215677d053cc7
Stefan Majewsky <stefan.majewsky@sap.com>
2024-05-10 13:53:30 +0200

    rewrite FindOrCreateRepository to prefer a plain SELECT
    The INSERT-first approach with ON CONFLICT DO NOTHING is often neat, but
    in this case, it does not even save queries.
    
    And what's worse, the INSERT always advances repo_id_seq even if no
    record ends up being inserted, so this always incurs a write in the DB
    even if we only read an existing repo.

--- internal/api/registry/fixtures/imagemanifest-003-after-upload-manifest-by-tag.sql
+++ internal/api/registry/fixtures/imagemanifest-003-after-upload-manifest-by-tag.sql
@@ -1,7 +1,7 @@
 INSERT INTO accounts (name, auth_tenant_id) VALUES ('test1', 'test1authtenant');
 
 INSERT INTO blob_mounts (blob_id, repo_id) VALUES (1, 1);
-INSERT INTO blob_mounts (blob_id, repo_id) VALUES (1, 6);
+INSERT INTO blob_mounts (blob_id, repo_id) VALUES (1, 2);
 
 INSERT INTO blobs (id, account_name, digest, size_bytes, storage_id, pushed_at, media_type, next_validation_at) VALUES (1, 'test1', 'sha256:712dfd307e9f735a037e1391f16c8747e7fb0d1318851e32591b51a6bc600c2d', 1102, '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b', 0, 'application/vnd.docker.container.image.v1+json', 604800);
 
@@ -14,7 +14,7 @@ INSERT INTO manifests (repo_id, digest, media_type, size_bytes, pushed_at, next_
 INSERT INTO quotas (auth_tenant_id, manifests) VALUES ('test1authtenant', 100);
 
 INSERT INTO repos (id, account_name, name) VALUES (1, 'test1', 'foo');
-INSERT INTO repos (id, account_name, name) VALUES (6, 'test1', 'bar');
+INSERT INTO repos (id, account_name, name) VALUES (2, 'test1', 'bar');
 
 INSERT INTO tags (repo_id, name, digest, pushed_at) VALUES (1, 'latest', 'sha256:8a9217f1887083297faf37cb2c1808f71289f0cd722d6e5157a07be1c362945f', 2);
 
