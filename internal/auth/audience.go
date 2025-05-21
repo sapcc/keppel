18b0ef765a6b043c0df68cbbed3ee50aa560e4f2
Stefan Majewsky <stefan.majewsky@sap.com>
2025-05-21 14:22:38 +0200

    update rightsholder name in copyright statements to recommended string

--- internal/auth/audience.go
+++ internal/auth/audience.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021 SAP SE
+// SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company
 // SPDX-License-Identifier: Apache-2.0
 
 package auth
--- internal/test/errors.go
+++ internal/test/errors.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2019 SAP SE
+// SPDX-FileCopyrightText: 2019 SAP SE or an SAP affiliate company
 // SPDX-License-Identifier: Apache-2.0
 
 package test
