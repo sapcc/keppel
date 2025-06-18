9f81d7b733085bdea6d9feb21c0c4a6276c977ca
Stefan Voigt <52136954+VoigtS@users.noreply.github.com>
2025-06-18 16:17:13 +0200

    Merge pull request #556 from sapcc/fix_modernize

--- internal/auth/request.go
+++ internal/auth/request.go
@@ -218,8 +218,7 @@ func checkBasicAuth(ctx context.Context, authHeader string, ad keppel.AuthDriver
 	userName, password := fields[0], fields[1]
 
 	// recognize peer credentials
-	if strings.HasPrefix(userName, "replication@") {
-		peerHostName := strings.TrimPrefix(userName, "replication@")
+	if peerHostName, ok := strings.CutPrefix(userName, "replication@"); ok {
 		peer, err := checkPeerCredentials(db, peerHostName, password)
 		if err != nil {
 			return nil, err
