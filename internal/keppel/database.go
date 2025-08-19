596c5d5d7350c8f3e01dfca2bc0e7cfe8a6a1003
Sandro Jäckel <sandro.jaeckel@sap.com>
2025-08-19 16:21:08 +0200

    Add constraint for next_check_at only being null when vuln_status is Rotten

--- internal/keppel/database.go
+++ internal/keppel/database.go
@@ -416,10 +416,14 @@ var sqlMigrations = map[string]string{
 		ALTER TABLE accounts DROP COLUMN rule_for_manifest;
 	`,
 	"053_change_vulnerability_report_to_allow_never_scanning_again.up.sql": `
-		ALTER TABLE trivy_security_info ALTER COLUMN next_check_at DROP NOT NULL;
+		ALTER TABLE trivy_security_info
+			ALTER COLUMN next_check_at DROP NOT NULL,
+			ADD CONSTRAINT next_check_at_only_null_when_rotten CHECK ((vuln_status = 'Rotten') = (next_check_at IS NULL));
 	`,
 	"053_change_vulnerability_report_to_allow_never_scanning_again.down.sql": `
-		ALTER TABLE trivy_security_info ALTER COLUMN next_check_at SET NOT NULL;
+		ALTER TABLE trivy_security_info
+			ALTER COLUMN next_check_at SET NOT NULL,
+			DROP CONSTRAINT next_check_at_only_null_when_rotten;
 	`,
 }
 
