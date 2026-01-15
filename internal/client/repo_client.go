sapcc/keppel

commit 14ad2e6ab0c770151612ef1714ae7fa2f57f676a

Use url builder

internal/client/repo_client.go
@@ -10,6 +10,8 @@ import (
 	"html"
 	"io"
 	"net/http"
+	"net/url"
+	"path"
 	"strings"
 
 	"maps"
@@ -67,10 +69,14 @@ func (c *RepoClient) doRequest(ctx context.Context, r repoRequest) (*http.Respon
 		c.Scheme = "https"
 	}
 
-	uri := fmt.Sprintf("%s://%s/v2/%s/%s", c.Scheme, c.Host, c.RepoName, r.Path)
+	uri := &url.URL{
+		Scheme: c.Scheme,
+		Host:   c.Host,
+		Path:   path.Join("v2", c.RepoName, r.Path),
+	}
 
 	// send GET request for manifest
-	resp, req, err := c.sendRequest(ctx, r, uri)
+	resp, req, err := c.sendRequest(ctx, r, uri.String())
 	if err != nil {
 		return nil, fmt.Errorf("during %s %s: %w", r.Method, uri, err)
 	}
@@ -94,7 +100,7 @@ func (c *RepoClient) doRequest(ctx context.Context, r repoRequest) (*http.Respon
 				return nil, err
 			}
 		}
-		resp, _, err = c.sendRequest(ctx, r, uri)
+		resp, _, err = c.sendRequest(ctx, r, uri.String())
 		if err != nil {
 			return nil, fmt.Errorf("during %s %s: %w", r.Method, uri, err)
 		}

commit 7798d025beb24c5fbfeb66cc80690976a597e5f7

fix regression introduced in 14ad2e6ab0c770151612ef1714ae7fa2f57f676a
RepoClient.UploadMonolithicBlob put a query string into the
repoRequest.Path attribute, so encoding the path broke the query string:

```
$ kc qa-de-2 keppel
$ k logs keppel-health-monitor-789597bcc5-6wzt9
2026/01/15 10:22:06 INFO: starting keppel-health-monitor 0a97bae
2026/01/15 10:22:09 FATAL: while uploading test image: during POST http://keppel-api.keppel.svc/v2/healthcheck-qa-de-2/healthcheck/blobs/uploads/%3Fdigest=sha256:54f06ec27d30c6b8cefd4610494cfc64daf1fff1ad905c6e81bcbbf61590d089: expected status 200, but got 405 Method Not Allowed
```

Note how the URL is `...uploads/%3Fdigest=...` instead of
`...uploads?digest=...`.

internal/client/repo_client.go
@@ -36,6 +36,7 @@ type RepoClient struct {
 type repoRequest struct {
 	Method       string
 	Path         string
+	Query        url.Values
 	Headers      http.Header
 	Body         io.ReadSeeker
 	ExpectStatus int
@@ -70,9 +71,10 @@ func (c *RepoClient) doRequest(ctx context.Context, r repoRequest) (*http.Respon
 	}
 
 	uri := &url.URL{
-		Scheme: c.Scheme,
-		Host:   c.Host,
-		Path:   path.Join("v2", c.RepoName, r.Path),
+		Scheme:   c.Scheme,
+		Host:     c.Host,
+		Path:     path.Join("v2", c.RepoName, r.Path),
+		RawQuery: r.Query.Encode(),
 	}
 
 	// send GET request for manifest

internal/client/upload.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"context"
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/opencontainers/go-digest"
@@ -19,7 +20,8 @@ func (c *RepoClient) UploadMonolithicBlob(ctx context.Context, contents []byte) 
 
 	resp, err := c.doRequest(ctx, repoRequest{
 		Method: "POST",
-		Path:   "blobs/uploads/?digest=" + d.String(),
+		Path:   "blobs/uploads/",
+		Query:  url.Values{"digest": []string{d.String()}},
 		Headers: http.Header{
 			"Content-Type": {"application/octet-stream"},
 		},