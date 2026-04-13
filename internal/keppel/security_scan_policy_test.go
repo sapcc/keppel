7542175ca274ea1e0878d48f22074a9b040f8c0e
Sandro Jäckel <sandro.jaeckel@sap.com>
2026-04-13 18:05:28 +0200

    Ignore IO related allocs, add SecurityScanPolicy to also test those code paths

--- internal/keppel/security_scan_policy_test.go
+++ internal/keppel/security_scan_policy_test.go
@@ -4,6 +4,8 @@
 package keppel
 
 import (
+	"bytes"
+	"io"
 	"os"
 	"testing"
 	"time"
@@ -11,14 +13,50 @@ import (
 	"github.com/sapcc/keppel/internal/trivy"
 
 	"github.com/sapcc/go-bits/must"
+	"github.com/sapcc/go-bits/regexpext"
 )
 
-func BenchmarkEnrichReport(b *testing.B) {
+func BenchmarkEnrichReportWithOneIgnore(b *testing.B) {
+	b.ReportAllocs()
+
+	reportContents := must.Return(os.ReadFile("../tasks/fixtures/trivy/report-vulnerable-with-fixes.json"))
+
+	b.ResetTimer()
 	for b.Loop() {
-		policies := SecurityScanPolicySet{}
+		policies := SecurityScanPolicySet{{
+			VulnerabilityIDRx: regexpext.BoundedRegexp(`CVE-2022-29458`),
+			Action: SecurityScanPolicyAction{
+				Assessment: "This is a test policy.",
+				Ignore:     true,
+			},
+		}}
 		report := trivy.ReportPayload{
 			Format:   "json",
-			Contents: must.Return(os.Open("../tasks/fixtures/trivy/report-vulnerable-with-fixes.json")),
+			Contents: io.NopCloser(bytes.NewReader(reportContents)),
+		}
+		_ = must.Return(policies.EnrichReport(&report, time.Now()))
+		must.Succeed(report.Contents.Close())
+	}
+}
+
+func BenchmarkEnrichReportExceptFixReleased(b *testing.B) {
+	b.ReportAllocs()
+
+	reportContents := must.Return(os.ReadFile("../tasks/fixtures/trivy/report-vulnerable-with-fixes.json"))
+
+	b.ResetTimer()
+	for b.Loop() {
+		policies := SecurityScanPolicySet{{
+			VulnerabilityIDRx: regexpext.BoundedRegexp(`CVE-2022-29458`),
+			Action: SecurityScanPolicyAction{
+				Assessment: "This is a test policy.",
+				Severity:   "Low",
+			},
+			ExceptFixReleased: true,
+		}}
+		report := trivy.ReportPayload{
+			Format:   "json",
+			Contents: io.NopCloser(bytes.NewReader(reportContents)),
 		}
 		_ = must.Return(policies.EnrichReport(&report, time.Now()))
 		must.Succeed(report.Contents.Close())
