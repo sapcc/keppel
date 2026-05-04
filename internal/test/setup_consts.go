0943524ab3b79e44ca7f6be7d262383cbecf4ad8
Sandro Jäckel <sandro.jaeckel@sap.com>
2026-05-04 13:37:56 +0200

    address review comments

--- internal/test/setup_consts.go
+++ internal/test/setup_consts.go
@@ -16,13 +16,13 @@ MC4CAQAwBQYDK2VwBCIEIMYjSqeiFo6zF62PYcnYtwEZMFu5GYfcYR7G0xQLQUMF
 -----END PRIVATE KEY-----`
 
 // UnitTestAnycastIssuerEd25519PrivateKey is an ed25519 private key that can be used as
-// KEPPEL_ANYCAST_ISSUER_KEY  in unit tests. DO NOT USE IN PRODUCTION.
+// KEPPEL_ANYCAST_ISSUER_KEY in unit tests. DO NOT USE IN PRODUCTION.
 const UnitTestAnycastIssuerEd25519PrivateKey = `-----BEGIN PRIVATE KEY-----
 MC4CAQAwBQYDK2VwBCIEIMk7vAS28DlAzYWG9yktmmAnla+wvvTo/Ah6qmXG6E+S
 -----END PRIVATE KEY-----`
 
 // UnitTestAnycastIssuerEd25519SecondPrivateKey is a second ed25519 private key that can be used as
-// KEPPEL_ANYCAST_ISSUER_KEY  in unit tests. DO NOT USE IN PRODUCTION.
+// KEPPEL_ANYCAST_ISSUER_KEY in unit tests. DO NOT USE IN PRODUCTION.
 const UnitTestAnycastIssuerEd25519SecondPrivateKey = `-----BEGIN PRIVATE KEY-----
 MC4CAQAwBQYDK2VwBCIEIChT9HxtygvQ6VzxIElJuDkLhtidVRn+30PNFIXd+78L
 -----END PRIVATE KEY-----`
